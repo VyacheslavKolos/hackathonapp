6e20f14353df65a12962f0b98db9e403ae98da0e
VyacheslavKolos <slavak0289@gmail.com>
2024-04-28 22:45:58 +0300

    changed domem

--- src/redux/constants/config.ts
+++ src/redux/constants/config.ts
@@ -1,3 +1,4 @@
-// @ts-ignore
 // eslint-disable-next-line import/prefer-default-export
-export const BASE_URL = import.meta.env.VITE_LOCAL_APP_URL || 'http://www.based-app.pp.ua';
+export const BASE_URL =
+  // @ts-ignore
+  import.meta.env.VITE_LOCAL_APP_URL || 'https://django-app-based-c03045a5f583.herokuapp.com/';
